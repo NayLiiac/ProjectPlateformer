2f918d340c0ca42282fed0a9eac20b3ebe8887b3
JaquyLaTortue <93046036+JaquyLaTortue@users.noreply.github.com>
2022-11-10 15:43:44 +0100

    Horizontal Move

--- Assets/Scripts/InputSystem/PlayerController.cs
+++ Assets/Scripts/InputSystem/PlayerController.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
+    Vector2 Movement = Vector2.zero;
+    Rigidbody2D Rb = null;
+    public float Speed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Rb == null)
+        {
+            return;
+        }
+        Rb.velocity = new Vector2(Movement.x * Speed , Rb.velocity.y);
+    }
+
+    public void OnMove(InputValue moveValue)
+    {
+        Movement = moveValue.Get<Vector2>();
     }
 }
