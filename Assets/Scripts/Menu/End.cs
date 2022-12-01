80aa4730968979a023ad6831b926436c2183c555
NayLiiac <kylian.levoyer@e-artsup.net>
2022-12-01 11:39:33 +0100

    Menu UI and Level Selector UI done

---
+++ Assets/Scripts/Menu/End.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    public string SceneName;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
