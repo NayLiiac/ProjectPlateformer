117ba24c0e239f3f7d2b38450fb100a476b70b41
NayLiiac <kylian.levoyer@e-artsup.net>
2022-12-02 21:20:10 +0100

    Death Count done, Respawn need some tweaks (we are stuck in the anormally death)

---
+++ Assets/Scripts/LevelDesign/DeathCount.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DeathCount : MonoBehaviour
+{
+    // Variable in order to count the number of deaths :
+
+    public TextMeshProUGUI Deathcount;
+    public int NumberOfDeaths = 0;
+
+    // Initialize number of deaths :
+    void Start()
+    {
+        Deathcount.text = NumberOfDeaths.ToString();
+    }
+
+
+    public void OnDeath()
+    {
+        PlayerController Pc = GetComponent<PlayerController>();
+        if (Pc.Health == 0)
+        {
+            NumberOfDeaths++;
+            
+        }
+        Deathcount.text = NumberOfDeaths.ToString();
+    }
+   
+}
